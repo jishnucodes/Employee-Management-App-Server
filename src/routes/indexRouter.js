09fafd8e7133cca819c9d4d0bfafb7b4be3521a4
Resma-Raghavan <resmaraghavan63@gmail.com>
2025-01-23 16:21:08 +0530

    merged master branch to Resma_Emp_API

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import userRouter from './userRoute.js';
 import roleRouter from './roleRouter.js';
-import employeeRouter from './employeeRoute.js';
-import departmentRouter from './departmentRoute.js';
 
 
 const router = express.Router();
 
 router.use('/user', userRouter);
+router.use('/role', roleRouter);
 
 export default router;
